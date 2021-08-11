2b6fdc2f9d4b5bd30ee981695f3b878d940fe2f7
Spartak-javohir <dellspartak00@gmail.com>
2021-08-11 12:45:21 +0500

    add

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+const { request } = require('express');
 const express = require('express');
+const path = require('path')
+const fs = require('fs').promises
 
 const server = express();
 
 server.listen(3030, ()=>{
     console.log('server');
+})
+server.get('/', async (request, response)=>{
+    const index = path.join(__dirname, 'views', 'index.html')
+    const red = await fs.readFile(index, 'utf-8')
+    response.sendFile(red)
+   
 })
